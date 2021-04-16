5f9d6f08e110a0f3ddf6aee48a46938a0a09e420
Anya Kovalieva <annakovalieva@gmail.com>
2021-04-16 13:40:35 +0200

    Tests for functions buttonRoll() and buttonPass() added. condition $_SESSION['total'][1] > 21 and  $_SESSION['total'][1] == 21 in buttonRoll not tested

--- test/Function/FunctionsTest.php
+++ test/Function/FunctionsTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Mos\Functions;
 
 use PHPUnit\Framework\TestCase;
+use Elmittil\Dice\Dice;
+use Elmittil\Dice\DiceHand;
 
 /**
  * Test cases for the functions in src/functions.php.
@@ -94,11 +96,128 @@ class FunctionsTest extends TestCase
     }
 
      /**
-     * Test the function destroySession().
-     * @runInSeparateProcess
+     * Test the function ButtonRoll.
+     * Empty session, 1 die
+     *  Does it roll the dice?
+     * Assert:
+     *      increases the value of $_SESSION["total"]
+     *      increases the value of $_SESSION["roll"]
+     *      
      */
-    public function buttonRoll()
+    public function testButtonRollRollsDice()
     {
-        
+        $diceQty = 1;
+        $_SESSION['roll'] = array(0, 0);
+        $_SESSION['score'] = array();
+        $_SESSION['total'] = array(0 , 0);
+        $_SESSION['message'] = "";
+
+        buttonRoll($diceQty);
+        $this->assertTrue($_SESSION['total'][0] > 0);
+        $this->assertTrue($_SESSION['roll'][0] > 0);
+    }
+
+    /**
+     * Test the function ButtonRoll.
+     * Empty session, 10 die (achieve $_SESSION["total"] > 21)
+     *  Does it roll the dice and change the message?
+     * Assert:
+     *      $_SESSION["message"] contains "WON"
+     *      increases the value of $_SESSION["total"]
+     *      increases the value of $_SESSION["roll"]
+     *      $_SESSION["score"] contains an entry "x" (WIN)
+     */
+    public function testButtonRollWinningMessage()
+    {
+        $diceQty = 10;
+        $_SESSION['roll'] = array(0, 0);
+        $_SESSION['score'] = array();
+        $_SESSION['total'] = array(0 , 0);
+        $_SESSION['message'] = "";
+
+        buttonRoll($diceQty);
+
+        $this->assertStringContainsString("WON", $_SESSION['message']);
+        $this->assertTrue($_SESSION['total'][0] > 0);
+        $this->assertTrue($_SESSION['roll'][0] > 0);
+        $this->assertContains("x", $_SESSION['score'][0]);
+
+    }
+
+
+    /**
+     * Test the function ButtonRoll for PLAYER WIN.
+     * Session contains a losing score for computer, 1 die 
+     * (achieve $_SESSION["total"][1] > 21)
+     * Assert:
+     *      $_SESSION["message"] contains "WON"
+     *      increases the value of $_SESSION["total"]
+     *      increases the value of $_SESSION["roll"]
+     *      $_SESSION["score"] contains an entry "x" (WIN)
+     */
+    public function testButtonRollPlayerWins()
+    {
+        $diceQty = 1;
+        $_SESSION['roll'] = array(0, 0);
+        $_SESSION['score'] = array();
+        $_SESSION['total'] = [
+            0 => 0,
+            1 => 22
+        ];
+        $_SESSION['message'] = "";
+
+        buttonRoll($diceQty);
+        $this->assertContains("", $_SESSION['score'][0]);
+    }
+
+    /**
+     * Test the function ButtonPass for COMPUTER WIN.
+     * Session contains a empty score, 1 die 
+     * Assert:
+     *      $_SESSION['total'][1] > 0
+     *      $_SESSION['roll'][1] > 0
+     *      $_SESSION['message'] contains "WON"
+     *      $_SESSION['score'][0] (player) contains "" (lost)
+     */
+    public function testButtonPassPlayerWins()
+    {
+        $diceQty = 1;
+        $_SESSION['roll'] = array(0, 0);
+        $_SESSION['score'] = array();
+        $_SESSION['total'] = [
+            0 => 0,
+            1 => 0
+        ];
+        $_SESSION['message'] = "";
+
+        buttonPass($diceQty);
+        $this->assertStringContainsString("WON", $_SESSION['message']);
+        $this->assertTrue($_SESSION['total'][1] > 0);
+        $this->assertTrue($_SESSION['roll'][1] > 0);
+        $this->assertContains("x", $_SESSION['score'][0]);
+    }
+
+    /**
+     * Test the function ButtonPass for PLAYER WIN.
+     * Session contains a empty score, 1 die 
+     * Assert:
+     *      $_SESSION['total'][1] > 0
+     *      $_SESSION['roll'][1] > 0
+     *      $_SESSION['message'] contains "WON"
+     *      $_SESSION['score'][0] (player) contains "" (lost)
+     */
+    public function testButtonPassPlayerWinsFakeScore()
+    {
+        $diceQty = 1;
+        $_SESSION['roll'] = array(0, 0);
+        $_SESSION['score'] = array();
+        $_SESSION['total'] = [
+            0 => 22,
+            1 => 23
+        ];
+        $_SESSION['message'] = "";
+
+        buttonPass($diceQty);
+        $this->assertContains("x", $_SESSION['score'][0]);
     }
 }
